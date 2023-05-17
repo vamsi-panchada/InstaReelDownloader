9b30349a511c4e01acd26794f1f998d4115fe97b
vamsi panchada <vamsivs58@gmail.com>
2023-05-17 16:05:15 +0530

    new commit

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
 import streamlit as st
-import instaloader
 
-def download_instagram_reel(url):
-    loader = instaloader.Instaloader()
-    try:
-        loader.download_post(url, target='#reels')
-        st.success("Instagram reel downloaded successfully!")
-    except Exception as e:
-        st.error(f"An error occurred while downloading the Instagram reel: {str(e)}")
+st.title('Instagram Reel Downloader')
+st.text('This application is developed by vamsi panchada.\nIt allows you to download reels using reel share link.')
 
-
-st.title("Instagram Reel Downloader")
-
-
-url = st.text_input("Enter Instagram Reel URL")
-
-
-if st.button("Download"):
-    if url:
-        download_instagram_reel(url)
-    else:
-        st.warning("Please enter a valid Instagram Reel URL.")
+url = st.text_input('Enter url', help='Please paste url here')
